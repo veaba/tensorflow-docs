03cae71927292a70056d2d85a972c9ae8bfc6ec5
veaba <908662421@qq.com>
2019-10-10 15:45:27 +0800

    update script

--- scripts/line_to_line.py
+++ scripts/line_to_line.py
@@ -1,7 +1,12 @@
-from config import appid
-from config import key
 import requests
 import hashlib
+import re
+import asyncio
+from config import appid
+from config import key
+from category import *
+
+
 
 url = "https://www.tensorflow.org/api_docs/python/"
 appid = appid  # TODO
@@ -16,8 +21,9 @@ def md5(content):
     return m.hexdigest()
 
 
-def baidu_api_translate(content):
+async def baidu_api_translate(content):
     if len(content) > 0:
+        await asyncio.sleep(1)
         baidu_api_url = "https://fanyi-api.baidu.com/api/trans/vip/translate"
         res = requests.post(
             baidu_api_url,
@@ -45,12 +51,138 @@ def baidu_api_translate(content):
     else:
         return ""
 
+# 匹配模式，匹配到则不翻译，返回true，false 翻译。判断是横杠：- 开头的，不翻译
+def match_mode(line):
 
-def read_line():
-    with open("../docs/All_Symbols.md", "r") as f:
+    if re.match('-',str.strip(line)):
+        return True
+    elif re.match('\\[',str.strip(line)):
+        return True  
+    elif re.match('`',str.strip(line)):
+        return True
+    elif re.match(r'return|def|print|if|else|elif|where|tf|with|<|>|\.|try|finally|@|import|int32',str.strip(line)):
+        return True 
+    elif re.match('# ',str.strip(line)):
+        return True
+    elif re.search(', ',line):
+        return True
+    elif re.search(r'\((.*?)#',str.strip(line)):
+        return True
+    elif re.search(r'=|0',line):
+        return True
+    elif re.search('\\*',str.strip(line)):
+        return True
+    elif re.search('\\[\\(',str.strip(line)):
+        return True   
+    # 如果存在单个字符(|) 不翻译    
+    elif str.strip(line)=='(' or str.strip(line) ==')' or str.strip(line)=='.':
+        return True   
+    # ,( 结尾    
+    elif re.search(r'[(,]$',str.strip(line)):
+        return True
+    elif re.search(r'__$',str.strip(line)):
+        return True
+    elif re.search(r'\)$',str.strip(line)):
+        return True
+    elif re.search(r'\]\.$',str.strip(line)):
+        return True
+    elif re.search(r'[_-]',str.strip(line)):
+        return True
+    return False    
+
+all_lines_en={
+
+}
+def read_line(path):
+    with open(path, "r",encoding="utf-8") as f:
         for line in f.readlines():
             if len(line) > 1:
-                print(line)
+                if not match_mode(line):
+                    all_lines_en[line]=""
+
+# 翻译写入到原文件
+def re_write_line(path):
+    # 存储读出来的
+    temp_save_list=[]
+    wait_save_list=[]
+    with open(path,'r',encoding='utf-8') as f:
+        temp_save_list=f.readlines()
+
+    # 替换之后的文件
+
+    for line in temp_save_list:
+        if line in all_lines_en:
+            line_str=line.replace(line,all_lines_en[line])
+            wait_save_list.append(line_str)
+            #  这里发生了什么,此时的all_lines_en 竟然没有数据
+            print("line:",line)
+            print("line_str:",line_str)
+            print("all_lines_en[line]:",line_str)
+        else:
+             wait_save_list.append(line)
+    # 复写文件
+    with open(path,'a',encoding='utf8') as f:
+        for line in wait_save_list:
+            f.write(line)
+    values_10= list(all_lines_en.values())[:10]
+    keys_10= list(all_lines_en.keys())[:10]
+    print("keys_10:",keys_10)
+    print("values_10:",values_10)
+
+# 总行数5459
+# 总字数250053万字
+
+TOTAL={
+    "STRING_COUNT":0
+}
 
 
-read_line()
+def total_str():
+    mask=0
+    loop=asyncio.get_event_loop()
+    for item in all_lines_en:
+        TOTAL['STRING_COUNT']+=len(item)
+        mask+=1
+        all_lines_en[item]=loop.run_until_complete(baidu_api_translate(item))
+        if mask>10:
+            break
+    print(TOTAL)
+    print("keys:",list(all_lines_en.keys())[:10])
+    print("value：",list(all_lines_en.values())[:10])
+    return all_lines_en
+
+
+def parent_path(parent, key_name,task):
+    file_path_re = parent + key_name
+    file_path=re.sub(r' ','_',file_path_re)
+    if not task:
+        read_line(file_path+'.md')
+    else:
+        # 根据数组写入
+        re_write_line(file_path+'.md')
+def handle(array, parent,task=None):
+
+    if type(array) != list:
+        return
+    for obj in array:
+        if type(obj) == dict:
+            key_name = "".join(obj.keys())
+            values = obj.values()
+            [item_list] = values or [[]]
+            if len(item_list) == 1:
+                [file_name] = item_list
+                parent_path(parent + key_name + "/", file_name,task)
+            else:
+                handle(item_list, parent + key_name + "/",task)
+        elif type(obj) == str:
+            parent_path(parent, obj,task)
+    
+
+def hello():
+    handle(category, "../docs/")
+    total_str()
+    print("有值吗：",list(all_lines_en.values())[:10])
+    handle(category, "../docs/",task=1)
+
+
+hello()
--- scripts/test.py
+++ scripts/test.py
@@ -31,8 +31,45 @@
 
 # print(re.sub(r'[.]','/',c))
 
-a=0
-if a:
-    print("不存在")
-elif  not a:
-    print("hah")
+b={
+    "## Primary symbols":"## 主要符号",
+    "## xx symbols":"## 主要一符号",
+    "## 33 symbols":"## 主要二符号",
+    "## vv symbols":"## 主要三符号",
+    "## 999 symbols":"## 主要三符号",
+}
+
+
+def read_lines():
+    with open('test.md','r') as f:
+        for line in f.readlines():
+            print(line)
+
+def write_line():
+    file_list=[]
+    with open("test.md",'r') as f:
+        file_list=f.readlines()
+        
+    print(file_list)
+    new_file_list=[]
+    for line in file_list:
+        new_line=line.replace('\n','')
+        if new_line in b:
+            line_str=new_line.replace(new_line,b[new_line])
+            new_file_list.append(line_str+'\n')
+        else:
+            new_file_list.append(line)   
+   
+    with open('test.md','a',encoding='utf8') as f:
+        f.write('\n')
+        for line in new_file_list:
+            f.write(line)
+
+write_line()
+
+print(list(b.values())[1:3])
+## 步骤
+### 把文本读出来
+### 存到list 里面去
+### 然后替换文本
+### 再一次w模式写入到源文件
