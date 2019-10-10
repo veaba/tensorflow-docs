a42a4274c9749ad639947b93080c26f517199557
veaba <908662421@qq.com>
2019-10-10 17:52:08 +0800

    update 2019年10月10日17:52:08

--- scripts/create_i18n.py
+++ scripts/create_i18n.py
@@ -24,7 +24,7 @@ def md5(content):
 async def baidu_api_translate(content):
     print(time.time())
     if len(content) > 0:
-        await asyncio.sleep(1)
+        await asyncio.sleep(1/10)
         baidu_api_url = "https://fanyi-api.baidu.com/api/trans/vip/translate"
         res = requests.post(
             baidu_api_url,
@@ -98,83 +98,26 @@ def clear_file(path):
     with open(path,'w',encoding="utf-8") as f:
         f.write('')
 
-def read_line(path):
+def create_i18n_dict_file(path):
     with open(path, "r",encoding="utf-8") as f:
         for line in f.readlines():
             if len(line) > 1:
                 if not match_mode(line):
                     all_lines_en[line]=""
-    clear_file('i18n.py')
-    with open('i18n.py','a',encoding="utf-8") as f:
+    clear_file('i18n_dict.py')
+    with open('i18n_dict.py','a',encoding="utf-8") as f:
         f.write('i18n={\n',)
+        # loop=asyncio.get_event_loop()loop.run_until_complete(baidu_api_translate(line))
         for line in all_lines_en.keys():
             new_line=line.replace('\n','').replace("'","\\'")
-            f.write("'"+new_line+"':'',"+'\n')
+            f.write("'"+new_line+"':'"+""+"',"+'\n')
         f.write('}')
-
-# 翻译写入到原文件
-def re_write_line(path):
-    # 存储读出来的
-    temp_save_list=[]
-    wait_save_list=[]
-    with open(path,'r',encoding='utf-8') as f:
-        temp_save_list=f.readlines()
-
-    # 替换之后的文件
-
-    for line in temp_save_list:
-        if line in all_lines_en:
-            if len(all_lines_en[line]):
-                line_str=line.replace(line,all_lines_en[line])
-                wait_save_list.append(line_str+'\n')
-                #  这里发生了什么,此时的all_lines_en 竟然没有数据
-                print("line:",line)
-                print("line_str:",line_str)
-                print("all_lines_en[line]:",line_str)
-        else:
-             wait_save_list.append(line)
-    # 清空文件         
-    with open(path,'w',encoding='utf8') as f:
-        f.write('')
-    # 复写文件
-    with open(path,'a',encoding='utf8') as f:
-        for line in wait_save_list:
-            f.write(line+'\n')
-    # values_10= list(all_lines_en.values())[:10]
-    # keys_10= list(all_lines_en.keys())[:10]
-    # print("keys_10:",keys_10)
-    # print("values_10:",values_10)
-
-# 总行数5459
-# 总字数250053万字
-
-TOTAL={
-    "STRING_COUNT":0
-}
-
-
-def total_str():
-    mask=0
-    loop=asyncio.get_event_loop()
-    for item in all_lines_en:
-        TOTAL['STRING_COUNT']+=len(item)
-        mask+=1
-        all_lines_en[item]=loop.run_until_complete(baidu_api_translate(item))
-        if mask>10:
-            break
-    print(TOTAL)
-    print("keys:",list(all_lines_en.keys())[:10])
-    print("value：",list(all_lines_en.values())[:10])
-
 
 def parent_path(parent, key_name,task):
     file_path_re = parent + key_name
     file_path=re.sub(r' ','_',file_path_re)
     if not task:
-        read_line(file_path+'.md')
-    else:
-        # 根据数组写入
-        re_write_line(file_path+'.md')
+        create_i18n_dict_file(file_path+'.md')
 def handle(array, parent,task=None):
 
     if type(array) != list:
@@ -193,10 +136,12 @@ def handle(array, parent,task=None):
             parent_path(parent, obj,task)
     
 
-def hello():
+def create_i18n_py():
+    time_start= time.time()
     handle(category, "../docs/")
-    # total_str()
-    # handle(category, "../docs/",task=1)
+    time_end=time.time()
+    print('创建 create_i18n_dict_file 所消耗时间：',time_end-time_start)
 
+    
 
-hello()
+create_i18n_py()
--- scripts/test.py
+++ scripts/test.py
@@ -31,45 +31,49 @@
 
 # print(re.sub(r'[.]','/',c))
 
-b={
-    "## Primary symbols":"## 主要符号",
-    "## xx symbols":"## 主要一符号",
-    "## 33 symbols":"## 主要二符号",
-    "## vv symbols":"## 主要三符号",
-    "## 999 symbols":"## 主要三符号",
-}
+# b={
+#     "## Primary symbols":"## 主要符号",
+#     "## xx symbols":"## 主要一符号",
+#     "## 33 symbols":"## 主要二符号",
+#     "## vv symbols":"## 主要三符号",
+#     "## 999 symbols":"## 主要三符号",
+# }
 
 
-def read_lines():
-    with open('test.md','r') as f:
-        for line in f.readlines():
-            print(line)
+# def read_lines():
+#     with open('test.md','r') as f:
+#         for line in f.readlines():
+#             print(line)
 
-def write_line():
-    file_list=[]
-    with open("test.md",'r') as f:
-        file_list=f.readlines()
+# def write_line():
+#     file_list=[]
+#     with open("test.md",'r') as f:
+#         file_list=f.readlines()
         
-    print(file_list)
-    new_file_list=[]
-    for line in file_list:
-        new_line=line.replace('\n','')
-        if new_line in b:
-            line_str=new_line.replace(new_line,b[new_line])
-            new_file_list.append(line_str+'\n')
-        else:
-            new_file_list.append(line)   
+#     print(file_list)
+#     new_file_list=[]
+#     for line in file_list:
+#         new_line=line.replace('\n','')
+#         if new_line in b:
+#             line_str=new_line.replace(new_line,b[new_line])
+#             new_file_list.append(line_str+'\n')
+#         else:
+#             new_file_list.append(line)   
    
-    with open('test.md','a',encoding='utf8') as f:
-        f.write('\n')
-        for line in new_file_list:
-            f.write(line)
+#     with open('test.md','a',encoding='utf8') as f:
+#         f.write('\n')
+#         for line in new_file_list:
+#             f.write(line)
 
-write_line()
+# write_line()
 
-print(list(b.values())[1:3])
+# print(list(b.values())[1:3])
 ## 步骤
 ### 把文本读出来
 ### 存到list 里面去
 ### 然后替换文本
 ### 再一次w模式写入到源文件
+
+from i18n import *
+
+print(i18n)
