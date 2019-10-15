c1bb9de863dc6bf49ceb2bd86dbe2f39605b6ead
veaba <908662421@qq.com>
2019-10-15 13:52:40 +0800

    test：添加deploy 测试

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,10 @@ fi
 if [ -n "${ACTIONS_DEPLOY_KEY}" ]; then
     echo "开始安装: ACTIONS_DEPLOY_KEY ..."
     SSH_DIR="/root/.ssh"
-    mkdir "${SSH_DIR}"
+    echo "aaa">"aaa"
+    ls
+    sudo mkdir "${SSH_DIR}"
+    ls 
     ssh-keyscan -t rsa github.com >"${SSH_DIR}/known_hosts"
     echo "${ACTIONS_DEPLOY_KEY}" >"${SSH_DIR}/id_rsa"
     chmod 400 "${SSH_DIR}/id_rsa"
