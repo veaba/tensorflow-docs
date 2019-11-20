9b6b0b23c2238faf95045aa2b664c53e95713c42
Godpu <908662421@qq.com>
2019-11-20 22:11:42 +0800

    add pool threading handerl vuepress build demo

---
+++ scripts/build_vuepress.py
@@ -0,0 +1,17 @@
+import os 
+from multiprocessing.pool import ThreadPool
+forders=os.listdir('../docs')
+forders =[item for item in forders if item!='.vuepress' and item!='All_Symbols.md' and item!='README.md']
+
+
+def fn(path):
+    print(path)
+    os.system('vuepress build ../dosc'+path)
+
+def go():
+    pool=ThreadPool(processes=16)
+    pool.map(fn,(path for path in forders))
+    pool.close()
+
+
+# go()
